bugfix: correctly calculate observed energy with factor of 1/2

qopen/core.py
@@ -174,7 +174,7 @@ def energy1c(data, rho, df, fs):
 
     :param data: velocity data (m/s)
     :param rho: density (kg/m**3)"""
-    return rho * (data ** 2 + (scipy.fftpack.hilbert(data)) ** 2) / 4 / df / fs
+    return rho * (data ** 2 + (scipy.fftpack.hilbert(data)) ** 2) / 2 / df / fs
 
 
 def observed_energy(stream, rho, df, fs=FS, tolerance=1):